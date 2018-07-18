6bbcf54c4bd1c2f5e664b1138a51521111667b9b
Jardel Weyrich <jweyrich@gmail.com>
2018-07-18 19:16:09 -0300

    Fix memory leaks.

--- common_main.h
+++ common_main.h
@@ -8,16 +8,17 @@
 
 int main(int argc, char *argv[]) {
 	options_t *options = parse_options(argc, argv);
+	int fd = -1;
 
 	if (options->input == NULL || options->output == NULL) {
 		usage();
-		exit(EXIT_FAILURE);
+		goto failed;
 	}
-
-	int fd = open(options->input, O_RDONLY);
+	
+	fd = open(options->input, O_RDONLY);
 	if (fd == -1) {
 		perror("open");
-		exit(EXIT_FAILURE);
+		goto failed;
 	}
 
 	const size_t filesize = fsize(options->input);
@@ -25,20 +26,27 @@ int main(int argc, char *argv[]) {
 	void *data = mmap(0, filesize, PROT_READ, MAP_SHARED, fd, 0);
 	if (data == MAP_FAILED) {
 		perror("mmap");
-		exit(EXIT_FAILURE);
+		goto failed;
 	}
 
 	close(fd);
+	fd = -1;
 
 	do_work(options, data, filesize);
 
 	int ret = munmap(data, filesize);
 	if (ret == -1) {
 		perror("munmap");
-		exit(EXIT_FAILURE);
+		goto failed;
 	}
 
 	free_options(options);
 
 	return EXIT_SUCCESS;
+failed:
+	if (options != NULL)
+		free_options(options);
+	if (fd != -1)
+		close(fd);
+	exit(EXIT_FAILURE);
 }
